6b0ea24e1183c97035f6ce6bdedf073656ce3b77
i1123581321 <gokinjolno112358@gmail.com>
2021-11-22 19:07:48 +0800

    initial commit

---
+++ src/main/java/anony/AnonyBackendApplication.java
@@ -0,0 +1,13 @@
+package anony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/anony/AnonyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package anony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
